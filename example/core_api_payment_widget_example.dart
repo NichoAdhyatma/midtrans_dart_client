36c8c10c575049d77e6f2fedc50e13db7b0cf304
iZenrix <fajrulf960@gmail.com>
2025-06-09 01:14:20 +0700

    feat: core api payment example

---
+++ example/core_api_payment_widget_example.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:midtrans_dart_client/models/request/transaction_request.dart';
+import 'package:midtrans_dart_client/widgets/core_api_payment/core_api_payment_view.dart';
+
+class CoreApiPaymentWidgetExample extends StatelessWidget {
+  const CoreApiPaymentWidgetExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CoreApiPaymentView(
+      transactionRequest: TransactionRequest(
+        transactionDetails: TransactionDetails(
+          orderId: 'order-${DateTime.now().millisecondsSinceEpoch}',
+          grossAmount: 20000,
+        ),
+        creditCard: CreditCard(
+          secure: true,
+        ),
+      ),
+      onSettlement: (paymentData) {
+        // Handle payment settlement
+        // navigate to success page
+        // SuccessPaymentPage(paymentData: paymentData);
+      },
+      onCancel: (paymentData) {
+        // Handle payment cancel
+        // navigate to cancel page
+        // CancelPaymentPage(paymentData: paymentData);
+      },
+      onExpire: (paymentData) {
+        // Handle payment expire
+        // navigate to expire page
+        // ExpirePaymentPage(paymentData: paymentData);
+      },
+    );
+  }
+}
