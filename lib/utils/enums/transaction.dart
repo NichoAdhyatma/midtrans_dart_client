2c7ca004333b4789f6bcd1dbf935cacee0a316c1
iZenrix <fajrulf960@gmail.com>
2025-06-08 23:12:49 +0700

    feat: transaction status enum

--- lib/utils/enums/transaction.dart
+++ lib/utils/enums/transaction.dart
@@ -1,3 +1,18 @@
 enum Acquirer { gopay }
 
 enum PaymentType { qris }
+
+enum TransactionStatus {
+  authorize,
+  capture,
+  settlement,
+  deny,
+  pending,
+  cancel,
+  refund,
+  partialRefund,
+  chargeback,
+  partialChargeback,
+  expire,
+  failure
+}
